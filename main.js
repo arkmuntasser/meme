a515770607b6068e1f445d0a7bcfbae161aa55ca
Breelan <breelanl@email.arizona.edu>
2015-05-16 18:18:06 -0700

    fixed main.js

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-<script language = "JavaScript">
+$(document).ready(function() {
+    function readURL(input, option) {
+        document.getElementById("bannerImg").style.display = "block";
 
-function readURL(input, option) {
-    document.getElementById("bannerImg").style.display = "block";
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
 
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
+            reader.onload = function(e) {
+                $(".preview").attr("style", "background-image: url(" + e.target.result + ")");
+                console.log(e.target.result);
+            }
 
-        reader.onload = function(e) {
-            $(".preview").attr("style", "background-image: url(" + e.target.result + ")");
-            console.log(e.target.result);
+            reader.readAsDataURL(input.files[0]);
         }
-
-        reader.readAsDataURL(input.files[0]);
     }
-} 
-</script >
+});
