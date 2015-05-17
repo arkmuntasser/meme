4347d3b1b0b800f9d924431dd032fad3f86fe79d
Breelan <breelanl@email.arizona.edu>
2015-05-16 19:19:35 -0700

    move readURL logic to main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     function readURL(input, option) {
-        document.getElementById("bannerImg").style.display = "block";
+        // document.getElementById("bannerImg").style.display = "block";
 
         if (input.files && input.files[0]) {
             var reader = new FileReader();
@@ -13,4 +13,20 @@ $(document).ready(function() {
             reader.readAsDataURL(input.files[0]);
         }
     }
+
+    //.change function on readURL
+    // input-image
+    // input-logo
+
+
+
+    $('input[name=img]').change(function() {
+        var x = document.getElementsByClassName("input-image");
+        readURL(x[0], "img");
+    });
+
+    $('input[name=logo]').change(function() {
+        var y = document.getElementsByClassName("input-logo");
+        readURL(y[0], "logo");
+    });
 });
