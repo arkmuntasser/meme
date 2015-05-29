6cb60bf2105a3269e977acf2407e2165eac96789
Amir R Muntasser <arkmuntasser@gmail.com>
2015-05-28 20:40:18 -0700

    responsive fix

--- main.js
+++ main.js
@@ -272,8 +272,10 @@ $(document).ready(function() {
     function dlCanvas() {
         var dt = c.toDataURL('image/png');
 
-        dt = dt.replace(/^data:image\/[^;]*/, 'data:application/octet-stream');
-        dt = dt.replace(/^data:application\/octet-stream/, 'data:application/octet-stream;headers=Content-Disposition%3A%20attachment%3B%20filename=meme.png');
+        if($(window).width() > 767) {
+          dt = dt.replace(/^data:image\/[^;]*/, 'data:application/octet-stream');
+          dt = dt.replace(/^data:application\/octet-stream/, 'data:application/octet-stream;headers=Content-Disposition%3A%20attachment%3B%20filename=meme.png');
+        }
 
         this.href = dt;
     };
