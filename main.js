a03530203e61782098fff3e6a172b9a2639d7d0d
Amir R Muntasser <arkmuntasser@gmail.com>
2015-05-29 00:29:41 -0700

    UI improvements

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
 $(document).ready(function() {
   $('body').on({
     'touchmove': function(e) {
+      var position = $(this).scrollTop();
+      if(position >= 65) {
+        if(!$(".container").hasClass("fix")) {
+          $(".container").addClass("fix");
+        }
+      } else {
+        if($(".container").hasClass("fix")) {
+          $(".container").removeClass("fix");
+        }
+      }
+    }
+  });
+
+  $(window).scroll(function() {
     var position = $(this).scrollTop();
     if(position >= 65) {
-      if(!$(".container").hasClass("fix")) {
+      if($(this).width() <= 767 && !$(".container").hasClass("fix")) {
         $(".container").addClass("fix");
       }
     } else {
-      if($(".container").hasClass("fix")) {
+      if($(this).width() <= 767 && $(".container").hasClass("fix")) {
         $(".container").removeClass("fix");
       }
     }
-  }
-});
+  });
 
   $(".close").click(function() {
     $(this).parent().toggle();
