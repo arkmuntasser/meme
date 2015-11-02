36e915cc73cd4e1e5973d3b0aad7b8a693bdd783
Amir R Muntasser <arkmuntasser@gmail.com>
2015-11-01 21:21:24 -0700

    remove sizing bs

--- main.js
+++ main.js
@@ -3,28 +3,6 @@ $(document).ready(function() {
     FastClick.attach(document.body);
   });
 
-  var getIOSWindowHeight = function() {
-    var zoomLevel = document.documentElement.clientWidth / window.innerWidth;
-    return window.innerHeight * zoomLevel;
-  };
-
-  var adjustHeight = function() {
-    var iosHeight = getIOSWindowHeight();
-    var height = $(window).height();
-    var initPaddingTop = height - iosHeight;
-    $(".container").attr("style", "height: " + iosHeight + "px; padding-top: " + initPaddingTop + "px;");
-    $(".mobile-download-area").attr("style", "height: " + iosHeight + "px; padding-top: " + initPaddingTop + "px;");
-    if($(".sidebar").css("height") === "auto") {
-
-    }
-  };
-
-  $(window).resize(function() {
-    adjustHeight();
-  });
-
-  adjustHeight();
-
   $(window).scroll(function() {
     var position = $(window).scrollTop();
   });
