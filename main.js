ef86ae8e7b1784bc50203243884a4a067f3c5635
Amir R Muntasser <arkmuntasser@gmail.com>
2015-11-01 20:07:52 -0700

    iOS height fix

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ $(document).ready(function() {
     FastClick.attach(document.body);
   });
 
+  var getIOSWindowHeight = function() {
+    var zoomLevel = document.documentElement.clientWidth / window.innerWidth;
+    return window.innerHeight * zoomLevel;
+  };
+
+  $(window).resize(function() {
+    $(".container").attr("style", "height: " + getIOSWindowHeight + "px;");
+  });
+
+  $(".container").attr("style", "height: " + getIOSWindowHeight + "px;");
+
   $(".close").click(function() {
     $(this).parent().toggle();
     $(this).parent().find("img").remove();
